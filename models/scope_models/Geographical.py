15c1cad788a13b02a609a081dc688b1c69125317
lberger1993 <berger.luciaeve@gmail.com>
2019-03-25 19:47:26 -0400

    Adds details

--- models/scope_models/Geographical.py
+++ models/scope_models/Geographical.py
@@ -1,2 +1,5 @@
+import requests
 class Geographical:
     def __init__(self):
+
+    # https://nominatim.openstreetmap.org/reverse?format=json&lat=52.5487429714954&lon=-1.81602098644987&zoom=18&addressdetails=1
